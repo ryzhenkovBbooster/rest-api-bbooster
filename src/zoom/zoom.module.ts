4a115953fb76a1d164dbb468d28791e3719b1006
kira <sellcringe@yandex.ru>
2024-06-20 10:30:24 +0300

    rest API version: 1.1 (debug module zoom)

--- src/zoom/zoom.module.ts
+++ src/zoom/zoom.module.ts
@@ -1,14 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ZoomController } from './zoom.controller';
 import { ZoomService } from './zoom.service';
 import {redisStore} from "cache-manager-redis-store";
 import {CacheModule} from "@nestjs/cache-manager";
 import {RedisModule} from "../redis/redis.module";
 import {RedisService} from "../redis/redis.service";
+import {AuthModule} from "../auth/auth.module";
 
 
 @Module({
-  imports: [RedisModule],
+  imports: [RedisModule, forwardRef(() => AuthModule)],
 
   controllers: [ZoomController],
   providers: [ZoomService, RedisService]
